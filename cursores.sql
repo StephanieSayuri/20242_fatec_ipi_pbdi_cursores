830f1e875a1a68312b447ffd232b20a947d87c88
Stephanie Sayuri <ssayuri8@gmail.com>
2024-10-28 23:45:39 -0300

    feat: solves question 1.3

--- cursores.sql
+++ cursores.sql
@@ -47,3 +47,10 @@ $$;
 
 -- 1.3 Faça uma pesquisa sobre o anti-pattern chamado RBAR - Row By Agonizing Row.
 -- Explique com suas palavras do que se trata.
+
+-- O anti-pattern RBAR (Row By Agonizing Row) é uma abordagem ineficiente para processar 
+-- dados em bancos de dados, onde cada linha é manipulada individualmente em vez de trabalhar 
+-- com várias linhas de uma vez. Isso resulta em um desempenho lento, uso excessivo de recursos e um 
+-- código mais complexo e difícil de entender. Para melhorar a eficiência, é preferível utilizar 
+-- comandos SQL que operem em conjuntos de dados, permitindo que as operações sejam realizadas 
+-- mais rapidamente e com um código mais claro.
